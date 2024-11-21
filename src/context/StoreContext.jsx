329f7b9027c7e01285713cb361d735d946024553
robert <robertmatt113@gmail.com>
2024-11-21 17:04:12 -0600

    feat: update url value on context

--- src/context/StoreContext.jsx
+++ src/context/StoreContext.jsx
@@ -5,7 +5,7 @@ export const StoreContext = createContext(null);
 
 const StoreContextProvider = (props) => {
   const [cartItems, setCartItems] = useState({});
-  const url = "http://localhost:4000";
+  const url = process.env.URL_FRONT_SECRET;
   const [token, setToken] = useState("");
   const [food_list, setFoodList] = useState([]);
 
