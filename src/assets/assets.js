b9b26c237db6734e95c33ddc3bc6a87239707a3b
robertomattus <robertmatt113@gmail.com>
2025-06-03 16:29:01 -0500

    feature: update footer aspect

--- src/assets/assets.js
+++ src/assets/assets.js
@@ -1,4 +1,5 @@
 import logo from "./logo.png";
+import logo_dark from "./logo_dark.png";
 import header_img from "./header_img.png";
 import search_icon from "./search_icon.png";
 import menu_1 from "./menu_1.png";
@@ -63,6 +64,7 @@ import un_checked from "./un_checked.png";
 
 export const assets = {
   logo,
+  logo_dark,
   header_img,
   search_icon,
   rating_starts,
